a9076ac120347fb5e9a6509a55d9747fc2ef8189
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-05-25 17:17:37 +0600

    Update index.js

--- lessons/Lesson14/index.js
+++ lessons/Lesson14/index.js
@@ -40,3 +40,61 @@ new Vue({
         // => { message: "goodbye", foo: "abc", bar: "def" }
     }
 });
+
+var mixin2 = {
+    created: function () {
+        console.log('вызван хук примеси')
+    }
+}
+
+new Vue({
+    mixins: [mixin2],
+    created: function () {
+        console.log('вызван хук компонента')
+    }
+})
+
+// => "вызван хук примеси"
+// => "вызван хук компонента"
+
+var mixin3 = {
+    methods: {
+        foo: function () {
+            console.log('foo')
+        },
+        conflicting: function () {
+            console.log('из примеси')
+        }
+    }
+};
+
+var vm = new Vue({
+    mixins: [mixin3],
+    methods: {
+        bar: function () {
+            console.log('bar')
+        },
+        conflicting: function () {
+            console.log('из самого компонента')
+        }
+    }
+});
+
+vm.foo(); // => "foo"
+vm.bar(); // => "bar"
+vm.conflicting(); // => "из самого компонента"
+
+// добавляем обработчик для пользовательской опции `myOption`
+Vue.mixin({
+    created: function () {
+        var myOption = this.$options.myOption
+        if (myOption) {
+            console.log(myOption)
+        }
+    }
+});
+
+new Vue({
+    myOption: 'hello!'
+});
+// => "hello!"
