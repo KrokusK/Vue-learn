2a77bc380bb3c784e34f34b1272d199f2d940617
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-05-27 13:03:51 +0600

    lesson16

--- lessons/Lesson16/index.js
+++ lessons/Lesson16/index.js
@@ -30,3 +30,43 @@ Vue.component('anchored-heading-render', {
 new Vue({
     el: '#example-2'
 });
+
+var getChildrenTextContent = function (children) {
+    return children.map(function (node) {
+        return node.children
+            ? getChildrenTextContent(node.children)
+            : node.text
+    }).join('')
+};
+
+Vue.component('anchored-heading-render-get-children-text', {
+    render: function (createElement) {
+        // создать id в kebab-case
+        var headingId = getChildrenTextContent(this.$slots.default)
+            .toLowerCase()
+            .replace(/\W+/g, '-')
+            .replace(/(^-|-$)/g, '')
+
+        return createElement(
+            'h' + this.level,
+            [
+                createElement('a', {
+                    attrs: {
+                        name: headingId,
+                        href: '#' + headingId
+                    }
+                }, this.$slots.default)
+            ]
+        )
+    },
+    props: {
+        level: {
+            type: Number,
+            required: true
+        }
+    }
+});
+
+new Vue({
+    el: '#example-3'
+});
