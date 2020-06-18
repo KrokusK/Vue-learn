41af72c18b033168aa0b63c3672e5887b143c274
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-06-18 16:20:35 +0600

    web

--- web/index.js
+++ web/index.js
@@ -1,11 +1,22 @@
-import Vue from 'vue'
-import App from './App'
+// Шаг 1: Создаём экземпляр Vue
+const Vue = require('vue')
+const app = new Vue({
+    template: `<div>Hello World</div>`
+})
 
-Vue.config.productionTip = false
+// Шаг 2: Создаём рендерер
+const renderer = require('vue-server-renderer').createRenderer()
 
-/* eslint-disable no-new */
-new Vue({
-    el: '#app',
-    template: '<App/>',
-    components: { App }
+// Шаг 3: Рендерим экземпляр Vue в HTML
+renderer.renderToString(app, (err, html) => {
+    if (err) throw err
+    console.log(html)
+    // => <div data-server-rendered="true">Hello World</div>
+})
+
+// с версии 2.5.0+, возвращает Promise если коллбэк не указан:
+renderer.renderToString(app).then(html => {
+    console.log(html)
+}).catch(err => {
+    console.error(err)
 })
